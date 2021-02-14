fb7418984c42084af8c0d2dbe2baf5da44af17ee
akselmadslien <aksel.madslien@teknologiporten.no>
2021-02-14 20:33:22 +0100

    datagenerator working and nearly finished

--- Project1-Backpropagation/datagenerator.py
+++ Project1-Backpropagation/datagenerator.py
@@ -5,11 +5,12 @@ from PIL import Image, ImageDraw
 
 class datagenerator:
 
-    def __init__(self, n, size, noise, centered='True'):
+    def __init__(self, n, size, noise, centered=True):
         self.n = n  #size of grid
         self.size = size    #size of dataset
         self.noise = noise
         self.centered = centered
+
 
     def generate(self):
         size = self.size
@@ -19,124 +20,102 @@ class datagenerator:
         for i in range(size):
             im = Image.new("1", (n, n), color=1)
             #draw = ImageDraw.Draw(im)
-            method = random.randint(1,2)
+            method = random.randint(1,4)
             if method == 1:
                 self.rectangle(im, n)
             if method == 2:
-                self.ellipse(im, n)
+                self.circle(im, n)
+            if method == 3:
+                self.lines(im, n)
+            if method == 4:
+                self.cross(im, n)
             
             images.append(im)
             labels[i] = method
+            #im.show()
             
 
     def rectangle(self, image, n):
         draw = ImageDraw.Draw(image)
         if self.centered:
-            x0 = random.randint(0,n//2-1)
-            y0 = random.randint(0,n//2-1)
-            x1 = n-x0-1
-            y1 = n-y0-1
-        else:
-            x0 = random.randint(0,n-3)
-            y0 = random.randint(0,n-3)
-            x0 = random.randint(x0+2,n-1)
-            y0 = random.randint(y0+2,n-1)
-
-        draw.rectangle((x0, y0, x1, y1), outline=0)
-        #return image
-
-    def ellipse(self, image, n):
-        draw = ImageDraw.Draw(image) 
-        if self.centered:
             x0 = random.randint(0,n//2-2)
             y0 = random.randint(0,n//2-2)
             x1 = n-x0-1
             y1 = n-y0-1
         else:
-            x0 = random.randint(0,n-4)
-            y0 = random.randint(0,n-4)
-            x0 = random.randint(x0+3,n-1)
-            y0 = random.randint(y0+3,n-1)
+            x = random.randint(3,n-1)
+            y = random.randint(3,n-1)
+            x0 = random.randint(0, n-x-1)
+            y0 = random.randint(0, n-y-1)
+            x1 = x0+x
+            y1 = y0+y
+
+        draw.rectangle((x0, y0, x1, y1), outline=0)
+        #return image
+
+    def circle(self, image, n):
+        draw = ImageDraw.Draw(image) 
+        if self.centered:
+            x0 = random.randint(0,n//2-3)
+            y0 = x0
+            x1 = n-x0-1
+            y1 = x1
+        else:
+            diameter = random.randint(3,n-1)
+            x0 = random.randint(0, n-diameter-1)
+            y0 = random.randint(0, n-diameter-1)
+            x1 = x0+diameter
+            y1 = y0+diameter
+
 
         draw.ellipse((x0, y0, x1, y1), outline=0)
         #return image
 
     def lines(self, image, n):
+        #horisontal lines
         draw = ImageDraw.Draw(image)
         #number of lines
         lines = random.randint(2,n//3)
         #position for the lines
         pos = random.sample(range(n), lines)
-        
+        for p in pos:
+            draw.line((0, p, n, p), width=1)
+
 
 
     def cross(self, image, n):
         draw = ImageDraw.Draw(image)
         if self.centered:
-            x0 = random.randint(0,n//2-2)
-            y0 = random.randint(0,n//2-2)
+            x0 = random.randint(0,n//2-3)
+            y0 = random.randint(0,n//2-3)
             x1 = n-x0-1
             y1 = n-y0-1
         else:
-            x0 = random.randint(0,n-4)
-            y0 = random.randint(0,n-4)
-            x0 = random.randint(x0+3,n-1)
-            y0 = random.randint(y0+3,n-1)
-        width = random.randint(1, min(y1-y0-1, x1-x0-1))
+            x = random.randint(3,n-1)
+            y = random.randint(3,n-1)
+            x0 = random.randint(0, n-x)
+            y0 = random.randint(0, n-y)
+            x1 = x0+x
+            y1 = y0+y
+
+        width = random.randint(1, min((y1-y0)//2, (x1-x0)//2))
         #draw horisontal line
-        draw.line((y0, (y0+y1)//2, y1, (y0+y1)//2), width=width)
+        draw.line((x0, (y0+y1)//2, x1, (y0+y1)//2), width=width)
         #draw vertical line
         draw.line(((x0+x1)//2, y0, (x0+x1)//2, y1), width=width)
 
-    """
-    def generate(self):
-        size = self.size
-        n = self.n
-        grid = np.zeros((size, n, n))
-        target = np.zeros(size)
-        for i in range(size):
-            method = random.randint(1,2)
-            # create grid (image)
-            if method == 1:
-                grid[i] = self.vlines(grid[i], n)
-            if method == 2:
-                grid[i] = self.hlines(grid[i], n)
-            if method == 3:
-                grid[i] = self.square(grid[i], n)
-            #insert noise to image
-
-            # add target class
-            target[i] = method
-        
-        return grid, target
-
+    def im2grid(self, images):
+        # images: list of images
+        # returns: array of grids representing all images
+        gridarray = np.zeros((self.size, self.n, self.n))
+        for im in range(self.size):
+           gridarray[im] = np.asarray(images[im]).astype(int)
+        return gridarray
     
-    def vlines(self, grid, n):
-        #number of lines
-        lines = random.randint(2,n//3)
-        #position for the lines
-        pos = random.sample(range(n), lines)
-        for p in pos:
-            grid[:][p] = 1
-        return grid
-
-    def hlines(self, grid, n):
-        #number of lines
-        lines = random.randint(2,n//3)
-        #position for the lines
-        pos = random.sample(range(n), lines)
-        for p in pos:
-            grid[p][:] = 1
-        return grid
-
-    def square(self, grid, n):
-        #TODO: make function
-        return grid
-
-gen = datagenerator(20, 3, 0)
-
-images = gen.generate()
-
-for i in range(3):
-    print image(images[i])
-"""
+    def grid2array(self, gridarray):
+        # gridarray: array of grids
+        # returns: array of arrays representing all images
+        arr = np.zeros((self.size, self.n * self.n))
+        for i in range(self.size):
+            arr[i] = gridarray[i].flatten()
+        return arr
